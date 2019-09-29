bf5c961d05d0ad6288c16359b82d984666502cb8
miaguirre22 <aguirremartinignacio@gmail.com>
2019-09-29 10:47:15 -0300

    limpiando archivos

--- console.ts
+++
@@ -1,7 +0,0 @@
-let nombre: string = 
-var edad=prompt('Ingrese su edad:','');
-document.write('Hola ');
-document.write(nombre);
-document.write(' asi que tienes ');
-document.write(edad);
-document.write(' años');
