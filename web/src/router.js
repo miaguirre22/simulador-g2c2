a07d60edf0a759211bf006a04207db76778faaf6
Lautaro Ramos <lautaro.ramos@innew.la>
2019-10-25 21:34:40 -0300

    quitar ruta innecesaria

--- web/src/router.js
+++ web/src/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import DefaultLayout from './layouts/Default.vue'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -16,11 +15,6 @@ export default new Router({
           path: '',
           name: 'home',
           component: Home
-        },
-        {
-          path: '/about',
-          name: 'about',
-          component: About
         }
       ]
     }
